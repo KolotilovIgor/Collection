3048d932bc6adf6f1bf3e83b292b52105a2eec0c
IgorK <gosha424@mail.ru>
2024-04-21 19:02:17 +0300

    Добавьте файлы проекта.

---
+++ Collection/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static int[,] labirynth1 = new int[,]
+    {
+        {1, 1, 1, 1, 1, 1, 1 },
+        {1, 0, 0, 0, 0, 0, 1 },
+        {1, 0, 1, 1, 1, 0, 1 },
+        {0, 0, 0, 0, 1, 0, 0 },
+        {1, 1, 0, 0, 1, 1, 1 },
+        {1, 1, 1, 0, 1, 1, 1 },
+        {1, 1, 1, 0, 1, 1, 1 }
+    };
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Количество выходов в лабиринте: " + CountExits());
+    }
+
+    static int CountExits()
+    {
+        int count = 0;
+        for (int i = 0; i < labirynth1.GetLength(0); i++)
+        {
+            for (int j = 0; j < labirynth1.GetLength(1); j++)
+            {
+                if (labirynth1[i, j] == 0 && (i == 0 || i == labirynth1.GetLength(0) - 1 || j == 0 || j == labirynth1.GetLength(1) - 1))
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
